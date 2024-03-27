76599bdfb8df37dd568ef3bd813ecb5142186d9a
neha-malvia <neha.malvia@freshworks.com>
2024-03-27 14:10:04 +0530

    remove unused changes

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Your code here
-    var action = document.getElementById("click-me");
-    action.addEventListener('click', function () {
-        console.log("click working");
-        document.getElementById("result").innerHTML ="I am clicked"
-    });
-    
-});
