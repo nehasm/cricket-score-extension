2ed8c57095a99c6ab9aecd81cf9532df4c9da108
neha-malvia <neha.malvia@freshworks.com>
2024-03-27 14:05:26 +0530

    save data changes

--- popup.js
+++ popup.js
@@ -1,18 +1,28 @@
-let isEnabled = false;
+document.addEventListener("DOMContentLoaded", async function () {
+  // Function to update button text based on isEnabled state
+  function updateButtonText(isEnabled) {
+    const toggleLoggingButton = document.getElementById("btnGetData");
+    toggleLoggingButton.textContent = isEnabled
+      ? "Stop Logging"
+      : "Start Logging";
+  }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const toggleLoggingButton = document.getElementById('btnGetData')
-  
-  toggleLoggingButton.addEventListener('click', function() {
+  let { isEnabled } = await chrome.storage.sync.get("isEnabled");
+  const toggleLoggingButton = document.getElementById("btnGetData");
+
+  updateButtonText(isEnabled);
+
+  toggleLoggingButton.addEventListener("click", function () {
     isEnabled = !isEnabled;
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.runtime.sendMessage({ action: "toggleLogging", isEnabled: isEnabled });
+    chrome.storage.sync.set({ isEnabled: isEnabled });
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.runtime.sendMessage({
+        action: "toggleLogging",
+        isEnabled: isEnabled,
+      });
     });
 
-    if (isEnabled) {
-      toggleLoggingButton.textContent = 'Stop Logging';
-    } else {
-      toggleLoggingButton.textContent = 'Start Logging';
-    }
+    updateButtonText(isEnabled);
   });
 });
