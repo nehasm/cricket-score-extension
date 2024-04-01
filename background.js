86333fe411e8d8b4f2bec927c7cf79bceb8fd302
sanketmunot <sanketmunot95@gmail.com>
2024-04-01 23:31:34 +0530

    refactor & changed for init flow

--- background.js
+++ background.js
@@ -1,68 +1,57 @@
 // background.js
 
-let loggingIntervalId = null;
+function getLatestScoresAndRender() {
+  // Your API fetch logic
+  fetch(
+    "https://hs-consumer-api.espncricinfo.com/v1/pages/matches/current?lang=en&latest=true"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const iplMatches = data.matches.filter(
+        (match) => match.series.slug === "indian-premier-league-2024"
+        // && match.stage === "RUNNING"
+      );
+      let allIplMatches = [];
+      for (let match of iplMatches) {
+        let currentTeamData = {
+          teamData: [],
+          statusText: match.statusText,
+        };
+        for (let team of match.teams) {
+          currentTeamData.teamData.push({
+            teamName: team.team.abbreviation,
+            score: team.score,
+            scoreInfo: team.scoreInfo,
+          });
+        }
+        allIplMatches.push(currentTeamData);
+      }
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.action === "toggleLogging") {
-    const { isEnabled } = message;
-    if (isEnabled) {
-      startCricket();
-    } else {
-      stopCricket();
-    }
-  }
-});
+      // Send the fetched data to the content script
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (tabs.length > 0) {
+          const tabId = tabs[0].id;
+          chrome.tabs.sendMessage(
+            tabId,
+            { action: "polling", data: allIplMatches }, // Include fetched data in the message
+            function (response) {
+              // Handle the response from the content script if needed
+            }
+          );
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching API:", error);
+    });
+}
+
+let loggingIntervalId = null;
 
 function startCricket() {
   stopCricket();
 
-  loggingIntervalId = setInterval(function () {
-    // Your API fetch logic
-    fetch(
-      "https://hs-consumer-api.espncricinfo.com/v1/pages/matches/current?lang=en&latest=true"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const iplMatches = data.matches.filter(
-          (match) => match.series.slug === "indian-premier-league-2024"
-        );
-        let allIplMatches = [];
-        for (let match of iplMatches) {
-          let currentTeamData = {
-            teamData: [],
-            statusText: match.statusText,
-          };
-          for (let team of match.teams) {
-            currentTeamData.teamData.push({
-              teamName: team.team.abbreviation,
-              score: team.score,
-              scoreInfo: team.scoreInfo,
-            });
-          }
-          allIplMatches.push(currentTeamData);
-        }
-
-        // Send the fetched data to the content script
-        chrome.tabs.query(
-          { active: true, currentWindow: true },
-          function (tabs) {
-            if (tabs.length > 0) {
-              const tabId = tabs[0].id;
-              chrome.tabs.sendMessage(
-                tabId,
-                { action: "polling", data: allIplMatches }, // Include fetched data in the message
-                function (response) {
-                  // Handle the response from the content script if needed
-                }
-              );
-            }
-          }
-        );
-      })
-      .catch((error) => {
-        console.error("Error fetching API:", error);
-      });
-  }, 5000);
+  loggingIntervalId = setInterval(getLatestScoresAndRender, 5000);
 }
 
 function stopCricket() {
@@ -70,12 +59,12 @@ function stopCricket() {
     clearInterval(loggingIntervalId);
     loggingIntervalId = null;
 
-    chrome.tabs.query({}, function(tabs) {
-      tabs.forEach(function(tab) {
+    chrome.tabs.query({}, function (tabs) {
+      tabs.forEach(function (tab) {
         chrome.tabs.sendMessage(
           tab.id,
           { action: "stopDisplayScore" },
-          function(response) {
+          function (response) {
             // Handle the response from the content script if needed
           }
         );
@@ -84,3 +73,16 @@ function stopCricket() {
   }
 }
 
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  const { isEnabled } = message;
+  if (message.action === "init" && isEnabled) {
+    getLatestScoresAndRender();
+  }
+  if (message.action === "toggleLogging") {
+    if (isEnabled) {
+      startCricket();
+    } else {
+      stopCricket();
+    }
+  }
+});
