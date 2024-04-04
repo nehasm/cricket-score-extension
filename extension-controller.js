d8921ab7458eea6b4d42589484295a19b2456ddb
nehasm <neha.malvia@somaiya.edu>
2024-04-05 02:18:47 +0530

    added react and logo changes

---
+++ extension-controller.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const ExtensionController = () => {
+  return (
+    <div>ExtensionController</div>
+  )
+}
+
+export default ExtensionController
+
+
+// document.addEventListener("DOMContentLoaded", async function () {
+//   // Function to update button text based on isEnabled state
+//   function updateButtonText(isEnabled) {
+//     const toggleLoggingButton = document.getElementById("btnGetData");
+//     toggleLoggingButton.textContent = isEnabled
+//       ? "Stop Logging"
+//       : "Start Logging";
+//   }
+
+//   let { isEnabled } = await chrome.storage.sync.get("isEnabled");
+//   const toggleLoggingButton = document.getElementById("btnGetData");
+
+//   updateButtonText(isEnabled);
+
+//   toggleLoggingButton.addEventListener("click", function () {
+//     isEnabled = !isEnabled;
+//     chrome.storage.sync.set({ isEnabled: isEnabled });
+
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//       chrome.runtime.sendMessage({
+//         action: "toggleLogging",
+//         isEnabled: isEnabled,
+//       });
+//     });
+
+//     updateButtonText(isEnabled);
+//   });
+// });
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,51 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: './popup.js',
+  entry: './index.js',
   mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'cheap-module-source-map',
+  resolve: {
+    extensions: [".js", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
     new CopyPlugin({
       patterns: [
-        { from: 'popup.html', to: path.resolve(__dirname, 'dist') },
         { from: 'manifest.json', to: path.resolve(__dirname, 'dist') },
         { from: 'contentScripts.js', to: path.resolve(__dirname, 'dist') },
         { from: 'background.js', to: path.resolve(__dirname, 'dist') },
         { from: 'live-score.css', to: path.resolve(__dirname, 'dist') },
+        { from: 'icon128.png', to: path.resolve(__dirname, 'dist') }
       ],
     }),
   ],
